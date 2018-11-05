3b016bc82c31ce022ad8b5c2df751ba7e20e59ae
stampedethegoat <ben.basuni@datadoghq.com>
2018-11-05 00:24:20 -0500

    views

--- app/server.js
+++ app/server.js
@@ -7,6 +7,8 @@ app.set('view engine', 'pug')
 
 app.get('/', async (req, res) => {
   console.log(`__dirname`, __dirname);
+  console.dir(config.root);
+  console.dir(path.join(config.root, 'server/views'))
   //let gameObj = await index.getReplayData();
   res.render('index', { title   : 'Hackday' })
 })
