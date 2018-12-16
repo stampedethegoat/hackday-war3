d8353e7a3bbd51549250aea3c11d9817384bf137
stampedethegoat <ben.basuni@datadoghq.com>
2018-12-15 18:25:59 -0800

    changed port

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const tracer = require('dd-trace').init()
 const app    = require('express')()
-const port   = process.env.PORT || 3000;
+const port   = process.env.PORT || 80;
 const index  = require('./index');
 
 app.set('views', __dirname + '/../views');
