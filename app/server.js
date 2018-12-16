6f0ae54ff3b47c12d3899474d8dba9da6a06ac9c
stampedethegoat <ben.basuni@datadoghq.com>
2018-12-15 18:38:40 -0800

    heroku testing

--- app/server.js
+++ app/server.js
@@ -1,14 +1,12 @@
-const port   = process.env.PORT || 80;
 const tracer = require('dd-trace').init()
 const app    = require('express')()
+const port   = process.env.PORT || 80;
 const index  = require('./index');
 
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'pug')
 
-app.get('/', async (req, res) => {
-  res.render('index', { title   : 'Hackday' })
-})
+app.get('/', async (req, res) => res.render('index', { title   : 'Hackday' }));
 
 app.get('/post', async (req, res) => {
   await index.submitMetrics();
