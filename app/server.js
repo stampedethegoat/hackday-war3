5d73177b674a220bf40809743474198785827eca
stampedethegoat <ben.basuni@datadoghq.com>
2018-12-15 18:16:13 -0800

    added node tracing library

--- app/server.js
+++ app/server.js
@@ -1,11 +1,14 @@
-const app  = require('express')()
-const port = process.env.PORT || 3000;
-const index = require('./index');
+const tracer = require('dd-trace').init()
+const app    = require('express')()
+const port   = process.env.PORT || 3000;
+const index  = require('./index');
 
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'pug')
 
-app.get('/', async (req, res) => res.render('index', { title   : 'Hackday' }))
+app.get('/', async (req, res) => {
+  res.render('index', { title   : 'Hackday' })
+)
 
 app.get('/post', async (req, res) => {
   await index.submitMetrics();
